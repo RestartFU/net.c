a4b72b07a4046c9738bc07dcef4023aa1383934d
Restart <45609733+RestartFU@users.noreply.github.com>
2024-04-01 15:54:42 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main() {
     addr.port = 1337;
 
     SOCKET sock;
-    if (!tcp_connect_addr(addr, &sock)) {
+    if (!tcp_connect(addr, &sock)) {
         printf("no success\n");
         exit(-1);
     }
